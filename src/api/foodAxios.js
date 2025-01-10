75bca7b2b84102751b0f37df411b28ca6449c8c9
leaxz <98172741+thaliafahira@users.noreply.github.com>
2025-01-10 21:31:09 +0700

    Update foodAxios.js

--- src/api/foodAxios.js
+++ src/api/foodAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const foodApi = axios.create({
-	baseURL: process.env.REACT_APP_API_BASE_URL,
+	baseURL: 'https://food-service-backend-production.up.railway.app/api',
 	headers: {
 		'Content-Type': 'application/json',
 	},
